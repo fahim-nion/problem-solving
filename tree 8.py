ff73846fdcd4b2c89a619d3138cc3fb907e5060c
fahim-nion <fahimalhassan4@gmail.com>
2025-05-18 17:46:17 +0600

    mid solve

--- tree 8.py
+++ tree 8.py
@@ -20,3 +20,24 @@ def tree_construction(arr, i = 1):
     p.right = tree_construction(arr, 2*i+1)
     return p
 
+#mid practice
+
+#counting nodes of the tree 
+
+def count(root):
+    if root == None:
+        return 0
+    else:
+        1 + count(root.left) + count(root.right)
+        
+#finding level of a Node
+def get_lvl(root,elem,root_lvl):
+    if root == None:
+        return 0
+    if root.elem == elem:
+        return root_lvl
+    down_lvl = get_lvl(root.left,elem,root_lvl +1)  #0
+    if down_lvl != 0:
+        return down_lvl
+    down_lvl = get_lvl(root.right,elem,root_lvl+1)
+    return down_lvl
